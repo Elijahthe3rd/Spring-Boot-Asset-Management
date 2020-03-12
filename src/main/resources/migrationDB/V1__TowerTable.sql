daf06dee5908a794c2e4f43d97df94672fe60e9a
Elijahthe3rd <sepuru.ne@yahoo.com>
2020-03-12 17:34:11 +0200

    added database configs
    Signed-off-by: Elijahthe3rd <sepuru.ne@yahoo.com>

---
+++ src/main/resources/migrationDB/V1__TowerTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tower (
+ id  NOT NULL PRIMARY KEY,
+ make VARCHAR(50) NOT NULL,
+ cpu VARCHAR(50) NOT NULL,
+ ram VARCHAR(50) NOT Null,
+ disk_type VARCHAR(50) NOT NULL,
+ disk_size VARCHAR(50) NOT NULL
+);
+
